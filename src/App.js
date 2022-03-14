7bcb671bd634f89be29d732b96354afa10de0ca9
곽윤구 <dbsrndi123@naver.com>
2022-03-14 15:53:32 +0900

    folder & data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import GlobalStyle from "./GlobalStyle";
+import GlobalStyle from './GlobalStyle';
+import Homepage from './pages/Homepage';
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Homepage />
     </>
   );
 }
