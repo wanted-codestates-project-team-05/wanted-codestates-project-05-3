7de5ea8ec6e4d9486b04116af90d73b0d260facc
곽윤구 <dbsrndi123@naver.com>
2022-03-14 20:53:22 +0900

    feat: click event ctrl shift trigger

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import GlobalStyle from './GlobalStyle';
-<<<<<<< HEAD
 import Homepage from './pages/Homepage';
-=======
-<<<<<<< HEAD
-import Setting from './components/Setting';
-import ListContainer from './components/ListContainer';
-import FakeHomePage from './components/ItemMoveButton/FakeHomePage';
-=======
-import Homepage from './pages/Homepage';
-import ListContainer from './components/ListContainer';
->>>>>>> 85ebc53 (feat: ListContainer component 기본)
->>>>>>> 79d3322 (feat: ListContainer component 기본)
 
 function App() {
   return (
     <>
       <GlobalStyle />
-<<<<<<< HEAD
       <Homepage />
-=======
-<<<<<<< HEAD
-      {/*<Homepage />*/}
-      {/*<ListContainer />*/}
-      {/* <FakeHomePage /> */}
-      <ListContainer />
-      <Setting />
-=======
-      <ListContainer />
->>>>>>> 85ebc53 (feat: ListContainer component 기본)
->>>>>>> 79d3322 (feat: ListContainer component 기본)
     </>
   );
 }
