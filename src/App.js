435641b44c7ff1fc9f0d71b26d0600def2b1e011
kyk1211 <85268135+kyk1211@users.noreply.github.com>
2022-03-14 20:12:57 +0900

    [Client]/Feat: ListContainer (#6)
    * feat: ListContainer component 기본

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import GlobalStyle from './GlobalStyle';
 import Homepage from './pages/Homepage';
+import ListContainer from './components/ListContainer';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Homepage />
+      <ListContainer />
     </>
   );
 }
---
+++ src/components/ListContainer.js
@@ -0,0 +1,128 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { emojiMenus } from '../assets/data';
+
+export default function ListContainer({ title = 'available', width, height, fontSize = 'M' }) {
+  const [items, setItems] = useState(emojiMenus);
+  const [filter, setFilter] = useState('');
+  const [selected, setSelected] = useState([]);
+  const [size, setSize] = useState();
+
+  const handleInput = ({ target: { value } }) => {
+    setFilter(value);
+  };
+
+  const handleClick = (item) => {
+    setSelected((prev) => (prev.includes(item) ? [...prev.filter((value) => value.id !== item.id)] : [...prev, item]));
+  };
+
+  useEffect(() => {
+    switch (fontSize) {
+      case 'XS':
+        setSize(0.6);
+        break;
+      case 'S':
+        setSize(0.8);
+        break;
+      default:
+        setSize(1);
+    }
+  }, [fontSize]);
+
+  useEffect(() => {
+    setSelected([]);
+    setItems(emojiMenus.filter((item) => item.name.includes(filter)));
+  }, [filter]);
+
+  return (
+    <Container width={width}>
+      <Input type="text" onChange={handleInput} placeholder={'search'} />
+      <Wrapper>
+        <TextBox>
+          <p>{title}</p>
+        </TextBox>
+        <Ul height={height}>
+          {items.map((item) => {
+            return (
+              <Li
+                key={item.id}
+                size={size}
+                selected={selected.includes(item)}
+                onClick={() => handleClick(item)}
+              >{`${item.emoji} ${item.name}`}</Li>
+            );
+          })}
+        </Ul>
+        <CountBox>
+          <p>
+            {selected.length}/{items.length}
+          </p>
+        </CountBox>
+      </Wrapper>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: ${({ width }) => (width ? width : 200)}px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin: auto;
+`;
+
+const Input = styled.input`
+  outline: none;
+  border: 1px solid lightgray;
+  border-radius: 5px;
+  height: 10px;
+  padding: 10px;
+`;
+
+const Wrapper = styled.div`
+  border: 1px solid lightgray;
+  border-radius: 5px;
+  height: 100%;
+`;
+
+const TextBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  font-weight: bold;
+  padding: 10px 0 10px 10px;
+  border-bottom: 1px solid lightgray;
+`;
+
+const Ul = styled.ul`
+  list-style: none;
+  height: ${({ height }) => (height ? height : 300)}px;
+
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const Li = styled.li`
+  padding: 10px 0 10px 15px;
+  font-size: ${({ size }) => size * 16}px;
+  border-bottom: 1px solid lightgray;
+  ${({ selected }) => (selected ? 'background-color: skyblue;' : '')}
+  cursor: pointer;
+  &:hover {
+    background-color: skyblue;
+  }
+  &:last-child {
+    border-bottom: none;
+  }
+`;
+
+const CountBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 500;
+  padding: 5px 0px;
+  border-top: 1px solid lightgray;
+`;
