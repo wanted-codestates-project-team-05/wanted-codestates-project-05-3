79d3322153f2cc84c7f4eba6b02493cedbd2f24c
곽윤구 <dbsrndi123@naver.com>
2022-03-14 17:25:10 +0900

    feat: ListContainer component 기본

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import GlobalStyle from './GlobalStyle';
+<<<<<<< HEAD
 import Setting from './components/Setting';
 import ListContainer from './components/ListContainer';
 import FakeHomePage from './components/ItemMoveButton/FakeHomePage';
+=======
+import Homepage from './pages/Homepage';
+import ListContainer from './components/ListContainer';
+>>>>>>> 85ebc53 (feat: ListContainer component 기본)
 
 function App() {
   return (
     <>
       <GlobalStyle />
+<<<<<<< HEAD
       {/*<Homepage />*/}
       {/*<ListContainer />*/}
       {/* <FakeHomePage /> */}
       <ListContainer />
       <Setting />
+=======
+      <ListContainer />
+>>>>>>> 85ebc53 (feat: ListContainer component 기본)
     </>
   );
 }
