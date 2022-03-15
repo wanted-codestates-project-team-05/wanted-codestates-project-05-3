57b0a033f321ef24bebf555017ac1b3ec3f9016a
곽윤구 <dbsrndi123@naver.com>
2022-03-15 14:35:53 +0900

    fix: app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import GlobalStyle from './GlobalStyle';
 import Homepage from './pages/Homepage';
-import ListContainer from './components/ListContainer';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <ListContainer />
+      <Homepage />
     </>
   );
 }
