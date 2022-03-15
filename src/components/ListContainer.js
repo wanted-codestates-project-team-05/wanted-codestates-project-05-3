07d9cfadef79a5aaddcdceacab510a522e4f664d
SeongHyun <70502670+Lee-Seonghyun316@users.noreply.github.com>
2022-03-15 11:33:17 +0900

    [Client] / #18, #19 / edit: HomePage, ItemMoveButton (#20)
    * Refactor: selected 상태 왼쪽 오른쪽 분리 관리
    
    * Refactor: 클릭 이벤트 아이콘 -> 버튼 이동, 전역 기본 상태 변경(멀티선택, 숫자 보이게)
    
    * Refactor: 동시에 왼쪽 오른쪽 리스트 선택 안되는 기능 추가
    
    * Refactor: commend 키 입력시 다중선택 기능 추가

--- src/components/ListContainer.js
+++ src/components/ListContainer.js
@@ -33,7 +33,7 @@ export default function ListContainer({ list, setList, selected, setSelected, ti
       setInitialSelect(item);
       return;
     }
-    if (e.nativeEvent.ctrlKey) {
+    if (e.nativeEvent.ctrlKey || e.metaKey) {
       setSelected((prev) =>
         prev.includes(item) ? [...prev.filter((value) => value.id !== item.id)] : [...prev, item]
       );
@@ -50,14 +50,6 @@ export default function ListContainer({ list, setList, selected, setSelected, ti
     setSelected([item]);
     setInitialSelect(item);
   };
-
-  // useEffect(() => {
-  //   console.log(selected);
-  // }, [selected]);
-
-  useEffect(() => {
-    console.log(list);
-  }, [list]);
 
   useEffect(() => {
     setSelected([]);
