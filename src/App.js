0c3ec20b5b06ee151bb9912516abfceb245a6b4b
곽윤구 <dbsrndi123@naver.com>
2022-03-15 11:18:50 +0900

    rebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import GlobalStyle from './GlobalStyle';
+<<<<<<< HEAD
+=======
+import Setting from './components/Setting';
+>>>>>>> 082d710 (rebase)
 import Homepage from './pages/Homepage';
 
 function App() {
@@ -6,6 +10,10 @@ function App() {
     <>
       <GlobalStyle />
       <Homepage />
+<<<<<<< HEAD
+=======
+      <Setting />
+>>>>>>> 082d710 (rebase)
     </>
   );
 }
