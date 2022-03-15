8ab3cf946bbc2fd613f7b05b9ac44442e0ace0e3
곽윤구 <dbsrndi123@naver.com>
2022-03-15 11:36:47 +0900

    fix: list move bug

--- src/atom/onoffAtom.js
+++ src/atom/onoffAtom.js
@@ -2,11 +2,7 @@ import { atom } from 'recoil';
 
 export const searchOnOffState = atom({
   key: 'searchOnOffState',
-<<<<<<< HEAD
   default: window.localStorage.getItem('searchOnOff') || true,
-=======
-  default: window.localStorage.getItem('searchOnOff') || false,
->>>>>>> 96ec85c ([Client] / #18, #19 / edit: HomePage, ItemMoveButton (#20))
 });
 
 export const titleOnOffState = atom({
@@ -16,18 +12,10 @@ export const titleOnOffState = atom({
 
 export const oneDragOnOffState = atom({
   key: 'oneDragOnOffState',
-<<<<<<< HEAD
   default: window.localStorage.getItem('oneDragOnOff') || true,
-=======
-	default: window.localStorage.getItem('oneDragOnOff') || true,
->>>>>>> 96ec85c ([Client] / #18, #19 / edit: HomePage, ItemMoveButton (#20))
 });
 
 export const itemNumOnOffState = atom({
   key: 'itemNumOnOffState',
-<<<<<<< HEAD
   default: window.localStorage.getItem('itemNumOnOff') || true,
-=======
-	default: window.localStorage.getItem('itemNumOnOff') || true,
->>>>>>> 96ec85c ([Client] / #18, #19 / edit: HomePage, ItemMoveButton (#20))
 });
