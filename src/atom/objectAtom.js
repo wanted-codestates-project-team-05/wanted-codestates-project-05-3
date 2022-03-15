6f5a22514be35ac8ff17a079dcaf50090a622237
ninjaskyu <2066apple@naver.com>
2022-03-15 18:23:29 +0900

    [Client] / #29 / Feat: 세션스토리지 훅 추가 및 테스트코드 삭제 (#30)
    * Feat: storage hooks 구현
    
    * Update: useSetLocalStorage.js 파일 위치 변경
    
    * Feat: 데이터 세션 스토리지 연결

--- src/atom/objectAtom.js
+++ src/atom/objectAtom.js
@@ -3,35 +3,35 @@ import { atom } from 'recoil';
 
 export const leftListState = atom({
 	key: 'leftListState',
-	default: window.localStorage.getItem('leftList') || emojiMenus,
+	default: JSON.parse(window.sessionStorage.getItem('leftList')) || emojiMenus,
 });
 
 export const rightListState = atom({
 	key: 'rightListState',
-	default: window.localStorage.getItem('rightList') || [],
+	default: JSON.parse(window.sessionStorage.getItem('rightList')) || [],
 });
 
 export const leftTitleNameState = atom({
 	key: 'leftTitleNameState',
-	default: window.localStorage.getItem('leftTitleName') || 'available options',
+	default: window.sessionStorage.getItem('leftTitleName') || 'available options',
 });
 
 export const rightTitleNameState = atom({
 	key: 'rightTitleNameState',
-	default: window.localStorage.getItem('rightTitleName') || 'selected options',
+	default: window.sessionStorage.getItem('rightTitleName') || 'selected options',
 });
 
 export const itemSizeState = atom({
 	key: 'itemSizeState',
-	default: window.localStorage.getItem('itemSize') || 16,
+	default: parseInt(window.sessionStorage.getItem('itemSize')) || 16,
 });
 
 export const componentWidthState = atom({
 	key: 'componentWidthState',
-	default: window.localStorage.getItem('componentWidth') || 200,
+	default: parseInt(window.sessionStorage.getItem('componentWidth')) || 200,
 });
 
 export const componentHeightState = atom({
 	key: 'componentHeightState',
-	default: window.localStorage.getItem('componentHeight') || 300,
+	default: parseInt(window.sessionStorage.getItem('componentHeight')) || 300,
 });
--- src/atom/onoffAtom.js
+++ src/atom/onoffAtom.js
@@ -1,21 +1,21 @@
 import { atom } from 'recoil';
 
 export const searchOnOffState = atom({
-  key: 'searchOnOffState',
-  default: window.localStorage.getItem('searchOnOff') || true,
+	key: 'searchOnOffState',
+	default: window.sessionStorage.getItem('searchOnOff') === 'false' ? false : true || true,
 });
 
 export const titleOnOffState = atom({
-  key: 'titleOnOffState',
-  default: window.localStorage.getItem('titleOnOff') || false,
+	key: 'titleOnOffState',
+	default: window.sessionStorage.getItem('titleOnOff') === 'true' ? true : false || false,
 });
 
 export const oneDragOnOffState = atom({
-  key: 'oneDragOnOffState',
-  default: window.localStorage.getItem('oneDragOnOff') || true,
+	key: 'oneDragOnOffState',
+	default: window.sessionStorage.getItem('oneDragOnOff') === 'false' ? false : true || true,
 });
 
 export const itemNumOnOffState = atom({
-  key: 'itemNumOnOffState',
-  default: window.localStorage.getItem('itemNumOnOff') || true,
+	key: 'itemNumOnOffState',
+	default: window.sessionStorage.getItem('itemNumOnOff') === 'false' ? false : true || true,
 });
